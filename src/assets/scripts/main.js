482f948283c9664e778698b07579e02504e14b72
rschmutz-orif <raphael.schmutz@orif.ch>
2025-10-27 10:02:56 +0000

    Ajout de l'injection JS des modules

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,18 +3,19 @@ async function injectBodyFromUrl(url, targetSelector) {
         const res = await fetch(url, { credentials: 'same-origin' }); // ou 'include' si besoin
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         const htmlText = await res.text();
-
-        // Parse le HTML en Document
+        console.log(htmlText)
+        // Parse le HTML
         const parser = new DOMParser();
         const doc = parser.parseFromString(htmlText, 'text/html');
 
-        // Récupère le contenu du body
-        const bodyHtml = doc.body.innerHTML;
-
-        // Injecte dans l'élément cible
+        // Sélectionne le conteneur cible
         const target = document.querySelector(targetSelector);
         if (!target) throw new Error('Cible introuvable: ' + targetSelector);
-        target.innerHTML = bodyHtml;
+
+        // Clone et ajoute chaque enfant du body
+        Array.from(doc.body.childNodes).forEach(node => {
+            target.appendChild(node.cloneNode(true));
+        });
 
     } catch (err) {
         console.error('Erreur injection HTML:', err);
